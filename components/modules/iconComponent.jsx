2968e1f844ec6bfcf0c15f04a049e5f4713b0994
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2023-03-15 17:04:24 +0700

    💄 update icon component

--- components/modules/iconComponent.jsx
+++ components/modules/iconComponent.jsx
@@ -5,7 +5,7 @@ import Heading from '../utils/heading'
 const IconComponent = () => {
   const subTitle = 'When it comes to herbal supplements, \n you are...'
   return (
-    <Container className="w-full h-full flex justify-center items-center">
+    <Container className="w-full h-full flex justify-center items-center py-10">
       <div className="w-full max-w-5xl flex flex-col items-center">
         <Heading
           title="About You"
@@ -13,7 +13,7 @@ const IconComponent = () => {
           subTitleSizeMobile="text-mheading1"
           classNameSubTitle="max-w-xs md:max-w-none"
         />
-        <div className="w-full flex flex-wrap justify-center gap-6">
+        <div className="w-full max-w-4xl flex flex-wrap justify-center gap-4">
           <ImageButton
             destination="/"
             src="/icons/informed1.png"
