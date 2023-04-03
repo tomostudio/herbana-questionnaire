bd86973ee04cfe72c2351ffc0a237a23bdfb4aa1
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2023-04-03 11:24:04 +0700

    💄 add metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,14 @@
 import Wrapper from './wrapper'
 import '@/styles/main.scss'
 
+export const metadata = {
+  title: {
+    default: 'Herbana',
+    template: '%s | Herbana',
+  },
+}
+
+
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
