6cefbbb6ebd3e042e44d1cb8ced01086820c3369
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2023-04-04 14:10:45 +0700

    💄 add seo

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,13 @@
 import Layout from '@/components/layout'
 import QuestionnairePage from '@/components/modules/questionnairePage'
+import quiz from './sample-data.json'
 
 export const metadata = {
-  title: 'Questionnaire',
+  title: quiz.data.seoData.title.en,
+  keywords: quiz.data.seoData.keyword.en.split(","),
+  openGraph: {
+    images: quiz.data.seoData.image,
+  },
 }
 
 export default function Home() {
