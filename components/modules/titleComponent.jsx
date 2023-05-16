2546687c8336d169fe1eb4f1b824a64b4a462ddb
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2023-05-16 10:32:05 +0700

    💄 update image rotation

--- components/modules/titleComponent.jsx
+++ components/modules/titleComponent.jsx
@@ -57,18 +57,18 @@ const TitleComponent = ({
         </RoundedFullButton>
       </div>
       <div className='px-6 max-w-screen-xl absolute mx-auto h-[50vh] min-h-[500px] max-h-[800px] md:h-auto w-full top-1/2 -translate-y-1/2 flex flex-row items-center justify-between'>
-        <div className='absolute -right-5 md:right-auto top-0 md:top-auto md:translate-y-0 md:relative w-44 h-44 md:w-60 md:h-60 lg:w-72 lg:h-72 rotate-[45deg] md:rotate-0 '>
+        <div className='absolute -right-5 md:right-auto top-0 md:top-auto md:translate-y-0 md:relative w-44 h-44 md:w-60 md:h-60 lg:w-72 lg:h-72 rotate-[-30deg] md:rotate-0 '>
           <Image
-            src={imageLeft}
+            src={imageRight}
             fill
             style={{
               objectFit: 'contain',
             }}
           />
         </div>
-        <div className='absolute -left-5 md:left-auto bottom-0 md:bottom-auto md:translate-y-0 md:relative w-44 h-44  md:w-60 md:h-60 lg:w-72 lg:h-72 rotate-[-135deg] md:rotate-0 '>
+        <div className='absolute -left-5 md:left-auto bottom-0 md:bottom-auto md:translate-y-0 md:relative w-44 h-44  md:w-60 md:h-60 lg:w-72 lg:h-72 rotate-[-60deg] md:rotate-0 '>
           <Image
-            src={imageRight}
+            src={imageLeft}
             fill
             style={{
               objectFit: 'contain',
