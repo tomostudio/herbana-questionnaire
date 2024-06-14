f4cfcb1d6a18969f4e7bac90a3014ee2d141768e
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2024-06-14 11:50:14 +0700

    💄 add analytics

--- app/wrapper.jsx
+++ app/wrapper.jsx
@@ -1,9 +1,23 @@
 'use client'
 
 import { AppWrapper } from 'context/state'
+import Script from 'next/script'
 
 const Wrapper = ({ children }) => {
-  return <AppWrapper>{children}</AppWrapper>
+  return (
+    <AppWrapper>
+      <Script>
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-FBD33KFSRE');
+`}
+      </Script>
+      {children}
+    </AppWrapper>
+  )
 }
 
 export default Wrapper
