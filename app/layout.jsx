37c45952be30375d00672357603b9a0d9bbcc56e
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2024-06-14 12:20:00 +0700

    💄 add: @next/third-parties for google analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import TOMOStudioHead from '@/components/utils/tomostudio-insert'
 import Wrapper from './wrapper'
 import '@/styles/main.scss'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata = {
   metadataBase: new URL('https://quiz.herbana.id'),
@@ -22,6 +23,7 @@ const RootLayout = ({ children }) => {
       <body>
         <Wrapper>{children}</Wrapper>
       </body>
+      <GoogleAnalytics gaId="G-FBD33KFSRE" />
     </html>
   )
 }
--- app/wrapper.jsx
+++ app/wrapper.jsx
@@ -6,15 +6,6 @@ import Script from 'next/script'
 const Wrapper = ({ children }) => {
   return (
     <AppWrapper>
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-FBD33KFSRE');
-`}
-      </Script>
       {children}
     </AppWrapper>
   )
